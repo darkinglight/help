5417b14c2d3505288f01fe87f669c885111b43ef
chenyuejun <chenyuejun@qiyi.com>
2020-05-08 14:03:49 +0800

    triple quick sort

---
+++ golang/algorithm/triquick.go
@@ -0,0 +1,51 @@
+//三相快排
+
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    datas := []int{3,5,8,2,3,6,5,2,4,6,2,7,2,34,45,2,56,43}
+    sort(datas, 0, len(datas) - 1)
+    fmt.Println(datas)
+}
+
+func sort(datas []int, lo int, hi int) {
+    if (lo >= hi) {
+        return
+    }
+
+    lt, gt := partition(datas, lo, hi)
+    sort(datas, lo, lt - 1)
+    sort(datas, gt + 1, hi)
+}
+
+func partition(datas []int, lo int, hi int) (int, int) {
+    lt := lo + 1
+    gt := hi
+    i := lo + 1
+    v := datas[lo]
+
+    for i <= gt {
+        if datas[i] == v {
+            i++
+        } else if (datas[i] < v) {
+            swap(datas, i, lt)
+            i++
+            lt++
+        } else {
+            swap(datas, i, hi)
+            hi--
+        }
+    }
+    swap(datas, lo, gt)
+    return lt, gt
+}
+
+func swap(datas []int, i int, j int) {
+    tmp := datas[i]
+    datas[i] = datas[j]
+    datas[j] = tmp
+}
