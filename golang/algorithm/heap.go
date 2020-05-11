49c7fa9d16ce7e1885fed45b5cfa4c698005e92b
chenyuejun <chenyuejun@qiyi.com>
2020-05-11 10:50:35 +0800

    heap modify

--- golang/algorithm/heap.go
+++ golang/algorithm/heap.go
@@ -1,5 +1,4 @@
 //堆排序
-
 package main
 
 import (
@@ -8,49 +7,43 @@ import (
 
 func main() {
     datas := []int{3,7,6,17,2,3,5,6}
-    sort(datas)
+    datas = sort(datas)
     fmt.Println(datas)
 }
 
-func sort(datas []int) {
+func sort(datas []int) []int {
     length := len(datas)
-    buildHeap(datas)
+
+    //datas[0] never used
+    datas = append([]int{0}, datas...)
+
+    for i := length / 2; i > 0; i-- {
+        sink(datas, i, length)
+    }
 
     for i := length; i > 1; i-- {
-        swap(datas, i - 1, 0)
-        verify(datas, 1, i - 1)
+        swap(datas, i, 1)
+        sink(datas, 1, i - 1)
     }
-}
 
-func buildHeap(datas []int) {
-    length := len(datas)
-    mid := length / 2
-
-    for i := mid; i > 0; i-- {
-        verify(datas, i, length)
-    }
+    //delete datas[0] add before
+    return datas[1:]
 }
 
 /**
  * index [1,MAX)
  */
-func verify(datas []int, index int, length int) {
-    left := index * 2
-    right := left + 1
-    if right <= length {
-        if datas[left - 1] < datas[right - 1] {
-            if datas[index - 1] < datas[right - 1] {
-                swap(datas, index - 1, right -1)
-                verify(datas, right, length)
-            }
-        } else {
-            if datas[index - 1] < datas[left - 1] {
-                swap(datas, index - 1, left - 1)
-                verify(datas, left, length)
-            }
-        }
-    } else if left <= length && datas[left - 1] > datas[index - 1] {
-        swap(datas, index - 1, left - 1)
+func sink(datas []int, index int, length int) {
+    sub := index * 2
+    if sub > length {
+        return
+    } else if sub + 1 <= length && datas[sub] < datas[sub + 1] {
+        sub++
+    }
+
+    if datas[index] < datas[sub] {
+        swap(datas, sub, index)
+        sink(datas, sub, length)
     }
 }
 
