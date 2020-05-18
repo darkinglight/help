62d280639b7d77796d8e1e50f1ccc60f1c48909f
chenyuejun <chenyuejun@qiyi.com>
2020-05-18 15:49:08 +0800

    batch over

--- golang/algorithm/bst.go
+++ golang/algorithm/bst.go
@@ -2,11 +2,17 @@
 package main
 
 import (
+    "errors"
     "fmt"
 )
 
 func main() {
-
+    b := &bst{}
+    b.put(2)
+    b.put(1)
+    b.put(3)
+    result := b.batch(0, 2)
+    fmt.Println(result)
 }
 
 type node struct {
@@ -17,7 +23,7 @@ type node struct {
 }
 
 // get num of node in this tree
-func size(n *node) {
+func size(n *node) int {
     if n == nil {
         return 0
     } else {
@@ -53,15 +59,16 @@ func get(n *node, rank int) *node {
     }
 }
 
-func min(n *node) *node {
-    if n == nil {
-        return nil
-    }
+func deleteMin(n *node) (*node, int) {
+    var deleteValue int
     if n.left != nil {
-        return min(n.left)
+        n.left, deleteValue = deleteMin(n.left)
+        n.N = size(n.left) + 1 + size(n.right)
     } else {
-        return n
+        deleteValue = n.value
+        n = n.right
     }
+    return n, deleteValue
 }
 
 func delete(n *node, value int) *node {
@@ -75,20 +82,53 @@ func delete(n *node, value int) *node {
         n.right = delete(n.right, value)
         return n
     }
-    //replace match node with right child's min node
     if n.right == nil {
         return n.left
     } else if n.left == nil {
         return n.right
+    }
+    //replace match node with right child's min node
+    node, min := deleteMin(n.right)
+    n.right = node
+    n.value = min
+    n.N = size(n.left) + 1 + size(n.right)
+
+    return n
+}
+
+func rank(n *node, value int) int {
+    if n == nil {
+        return -1
+    }
+    if n.value == value {
+        return size(n.left)
+    } else if value < n.value {
+        return rank(n.left, value)
     } else {
-        
+        return rank(n.right, value) + size(n.left) + 1
     }
 }
 
+func batch(n *node, lo, hi int) []int {
+    if n == nil || n.N <= lo || hi < 0 {
+        return []int{}
+    }
+    index := size(n.left)
+    result := []int{}
+    leftResult := batch(n.left, lo, hi)
+    result = append(result, leftResult...)
+    if lo <= index && index <= hi {
+        result = append(result, n.value)
+    }
+    rightResult := batch(n.right, lo - index - 1, hi - index - 1)
+    result = append(result, rightResult...)
+    return result
+}
+
 type search interface {
-    put(value int) error
+    put(value int)
     get(rank int) int
-    delete(value int) (*node, error)
+    delete(value int)
     rank(value int) int
     batch(lo int, hi int) []int
 }
@@ -101,7 +141,23 @@ func (b *bst) put(value int) {
     b.root = put(b.root, value)
 }
 
-func (b *bst) get(rank int) int {
+func (b *bst) get(rank int) (int, error) {
     n := get(b.root, rank)
-    return n == nil ? nil : n.value
+    if n == nil {
+        return -1, errors.New("value not exist")
+    } else {
+        return n.value, nil
+    }
+}
+
+func (b *bst) delete(value int) {
+    b.root = delete(b.root, value)
+}
+
+func (b *bst) rank(value int) int {
+    return rank(b.root, value)
+}
+
+func (b *bst) batch(lo, hi int) []int {
+    return batch(b.root, lo, hi)
 }
