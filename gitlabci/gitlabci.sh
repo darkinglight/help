9ba59a21775fd73b570c929ee839a86467ae8b61
chenyuejun <chenyuejun@eastmoney.com>
2022-01-08 17:18:33 +0800

    add gitlabci

---
+++ gitlabci/gitlabci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Start register gitlab ci runner !"
+
+docker run --rm -v gitlab-runner-config:/etc/gitlab-runner gitlab/gitlab-runner:latest register \
+	--non-interactive \
+	--executor "docker" \
+	--docker-image docker:20 \
+	--url "http://10.228.11.11/" \
+	--registration-token "siXuGkXkYFKcQxW_pyyn" \
+	--description "docker-runner" \
+	--tag-list "docker,aws" \
+	--run-untagged="true" \
+	--locked="false" \
+	--access-level="not_protected" \
+#	--template-config config.template.toml \
+	--config config.toml
