646883d26b69e0a3209efddefc80ba1966dc772d
陈岳军 <chenyuejun@eastmoney.com>
2023-07-12 19:43:28 +0800

    add score

--- python/baostock/modules.py
+++ python/baostock/modules.py
@@ -8,7 +8,7 @@ from profit import profit
 from priceinfo import priceinfo
 
 if __name__ == '__main__':
-    res = pd.DataFrame(columns=('name','growth','pe','peg','roe2022', "roeAvg"))
+    res = pd.DataFrame(columns=('name','growth','pe','peg','roe2022', "roeAvg", "score"))
     hs300 = hs300()
     year = 2022
     quarter = 4
@@ -41,6 +41,12 @@ if __name__ == '__main__':
             roeAvg = (profit2019['roeAvg'] + profit2020['roeAvg'] + profit2021['roeAvg'] + profit2022['roeAvg']) * 100 / 4
             res = res._append({'name': name, 'growth': round(avgEarning,2), 'pe': round(pe,2), 'peg': round(peg,2), 'roe2022': round(roe2022,2), 'roeAvg': round(roeAvg,2)}, ignore_index=True)
     res = res.sort_values(by = "roeAvg", ascending = False)
+
+    score = 0
+    for index, row in res.iterrows():
+        score += 1
+        res['score'][index] = score
+
     print(res)
     res.to_csv("dump.csv", encoding='utf-8')
     #res.plot.bar()
