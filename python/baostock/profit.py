3ee898097b8c7f8eb9d2e56b23b22702dae456e9
陈岳军 <chenyuejun@eastmoney.com>
2023-07-13 10:14:56 +0800

    profit remove redundent code

--- python/baostock/profit.py
+++ python/baostock/profit.py
@@ -9,24 +9,18 @@ if os.path.exists(filename):
 
 def profit(code, year, quarter):
     global df
-    result = df.loc[(df["code"] == code) & (df["year"] == year)]
-    if result.shape[0] > 0:
-        return result.iloc[0,:]
-
-    itemfilename = "data/profile_{}_{}_{}.csv".format(code,year,quarter)
-    if os.path.exists(itemfilename):
-        item = pd.read_csv(itemfilename)
-    else:
+    item = df.loc[(df["code"] == code) & (df["year"] == year)]
+    if result.shape[0] <= 0:
         rs_profit = bs.query_profit_data(code, year=year, quarter=quarter)
         print("call query_profit-data api", code, year, quarter)
         data_list = []
         while(rs_profit.error_code == '0') & rs_profit.next():
             data_list.append(rs_profit.get_row_data())
         item = pd.DataFrame(data_list, columns=rs_profit.fields)
-    item.insert(1, 'year', year)
-    item.insert(2, 'quarter', quarter)
-    df = df.append(item)
-    df.to_csv(filename, encoding="utf-8", index=False)
+        item.insert(1, 'year', year)
+        item.insert(2, 'quarter', quarter)
+        df = df.append(item)
+        df.to_csv(filename, encoding="utf-8", index=False)
     return item.iloc[0,:]
 
 if __name__ == "__main__":
