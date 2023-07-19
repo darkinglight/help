703889c709ad7024238b7d88024a22708405b935
陈岳军 <chenyuejun@eastmoney.com>
2023-07-19 20:01:54 +0800

    ...

--- python/baostock/modules.py
+++ python/baostock/modules.py
@@ -12,14 +12,14 @@ from priceinfo import priceinfo
 def getRoeAvg(profit):
 
     if type(profit['roeAvg']) != 'str':
-        return float(profit['roeAvg'])
+        return float(profit['roeAvg']) * 100
     elif len(profit['roeAvg']) <= 0:
         return 0
     else:
         return round(float(profit['roeAvg']) * 100,2)
 
 if __name__ == '__main__':
-    lg = bs.login()
+    #lg = bs.login()
     res = pd.DataFrame(columns=('name','netProfit2019','netProfit2022','growth','pe','peg','roe2019','roe2020','roe2021','roe2022', "roeAvg", "score"))
     hs300 = hs300()
     zz500 = zz500()
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     quarter = 4
     date = "2023-07-05"
     for index, row in zz800.iterrows():
-        if index < 900:
+        if index < 500:
             code = row['code']
 
             base = baseinfo(code)
@@ -68,7 +68,10 @@ if __name__ == '__main__':
     res = res.sort_values(by = "score", ascending = True)
     res.to_csv("dump.csv", encoding='utf-8')
     plt.rcParams['font.family'] = 'Microsoft YaHei'
-    res = res.head(n = 10)
-    res.plot(x = 'name', y = 'roeAvg')
+    res = res.set_index('name')
+    res = res.head(n = 20)
+    ax = res.plot(y = 'roeAvg')
+    res.plot(y='pe', ax=ax)
+    ax.legend()
     plt.show()
-    bs.logout()
+    #bs.logout()
