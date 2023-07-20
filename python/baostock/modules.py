5ce16427985c6582f6e80c5d7473d841d7b54aa4
陈岳军 <chenyuejun@eastmoney.com>
2023-07-20 15:15:05 +0800

    modify

--- python/baostock/modules.py
+++ python/baostock/modules.py
@@ -19,8 +19,8 @@ def getRoeAvg(profit):
         return round(float(profit['roeAvg']) * 100,2)
 
 if __name__ == '__main__':
-    lg = bs.login()
-    res = pd.DataFrame(columns=('name','netProfit2019','netProfit2022','growth','pe','peg','roe2019','roe2020','roe2021','roe2022', "roeAvg", "score"))
+    #lg = bs.login()
+    res = pd.DataFrame(columns=('name','netProfit2019','netProfit2022','growth','pe','roe2019','roe2020','roe2021','roe2022', "roeAvg", "roeMin","pb","roe/pb", "score"))
     hs300 = hs300()
     zz500 = zz500()
     zz800 = pd.concat([hs300,zz500], ignore_index=True)
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     quarter = 4
     date = "2023-07-05"
     for index, row in zz800.iterrows():
-        if index < 900:
+        if index < 600:
             code = row['code']
 
             base = baseinfo(code)
@@ -36,6 +36,7 @@ if __name__ == '__main__':
 
             price = priceinfo(code, date)
             pe = round(float(price.loc['peTTM']),2)
+            pb = round(float(price.loc['pbMRQ']),2)
             
             profit2022 = profit(code, year, quarter)
             roe2022 = getRoeAvg(profit2022)
@@ -47,11 +48,15 @@ if __name__ == '__main__':
             profit2019 = profit(code, year - 3, quarter)
             roe2019 = getRoeAvg(profit2019)
             netProfit2019 = round(float(profit2019['netProfit']),2)
-            item = pd.Series({'name':name,'netProfit2019':netProfit2019,'netProfit2022':netProfit2022,'pe':pe,'roe2019':roe2019,'roe2020':roe2020,'roe2021':roe2021,'roe2022':roe2022})
+            growth = round((netProfit2022 / netProfit2019 * 100 - 100)/4, 2)
+            item = pd.Series({'name':name,'netProfit2019':netProfit2019,'netProfit2022':netProfit2022, 'growth':growth,'pe':pe,'roe2019':roe2019,'roe2020':roe2020,'roe2021':roe2021,'roe2022':roe2022,'pb':pb})
             res = pd.concat([res,item.to_frame().T], ignore_index=True)
     # 过滤负分记录
-    res = res.loc[(res["roe2019"] > 0) & (res["roe2020"] > 0) & (res["roe2021"] > 0) & (res["roe2022"] > 0) & (res["pe"] > 0)]
+    minRoe = 10
+    res = res.loc[(res["roe2019"] > minRoe) & (res["roe2020"] > minRoe) & (res["roe2021"] > minRoe) & (res["roe2022"] > minRoe) & (res["pe"] > minRoe) & (res["pe"] > 0) & (res["pe"] < 30) & (res["growth"] > 5)]
     res['roeAvg'] = res[['roe2019','roe2020','roe2021','roe2022']].mean(1)
+    res['roeMin'] = res[['roe2022','roeAvg']].min(1)
+    res['roe/pb'] = res['roeMin'] / res['pb']
     # roe打分
     res = res.sort_values(by = "roeAvg", ascending = False)
     score = 0
@@ -67,11 +72,11 @@ if __name__ == '__main__':
     
     res = res.sort_values(by = "score", ascending = True)
     res.to_csv("dump.csv", encoding='utf-8')
-    plt.rcParams['font.family'] = 'Microsoft YaHei'
-    res = res.set_index('name')
-    res = res.head(n = 20)
-    ax = res.plot(y = 'roeAvg')
-    res.plot(y='pe', ax=ax)
-    ax.legend()
-    plt.show()
-    bs.logout()
+    #plt.rcParams['font.family'] = 'Microsoft YaHei'
+    #res = res.set_index('name')
+    #res = res.head(n = 20)
+    #ax = res.plot(y = 'roeAvg')
+    #res.plot(y='pe', ax=ax)
+    #ax.legend()
+    #plt.show()
+    #bs.logout()
