ebc1fb71ba108caff3efd248b5270fb7c7d9752b
light <light@lights-MBP.lan>
2023-07-25 23:07:15 +0800

    ...

--- python/baostock/cashflow.py
+++ python/baostock/cashflow.py
@@ -11,11 +11,12 @@ def cashflow(code):
     global df
     item = df.loc[(df["code"] == code)]
     if item.shape[0] <= 0:
-        rs_cash_flow = bs.query_cash_flow_data(code)
+        rs_cash_flow = bs.query_cash_flow_data(code,year='2023',quarter='1')
         data_list = []
         while(rs_cash_flow.error_code == '0') & rs_cash_flow.next():
             data_list.append(rs_cash_flow.get_row_data())
         item = pd.DataFrame(data_list, columns=rs_cash_flow.fields)
+        print(item)
         df = pd.concat([df,item])
         df.to_csv(filename, encoding="utf-8", index=False)
     if item.shape[0] <= 0:
@@ -27,7 +28,15 @@ def cashflow(code):
     return item.iloc[0]
 
 if __name__ == "__main__":
+    from allstock import allstock
+    import sys
     lg = bs.login()
-    result = cashflow("sh.600009")
-    print(result)
+    allstock = allstock()
+    for index, row in allstock.iterrows():
+        if index < 10000:
+            code = row['code']
+            cashflow(code)
+            print(code)
     bs.logout()
+
+
